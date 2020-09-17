d2390b25a4354397397016af346fa6ec53a3cfa7
Rildo Alves <junior.alvesp99@gmail.com>
2020-09-17 01:19:49 -0300

    Criando componente card

--- web-app/src/app/card/card.component.ts
+++ web-app/src/app/card/card.component.ts
@@ -7,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+public imagem = 'https://upload.wikimedia.org/wikipedia/en/0/03/Walter_White_S5B.png';
+public exibe = false;
+public descricao = 'Say my name';
+public usuario = 'Heisenberg';
+public profissao = 'Professor de Química';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  
+  public exibirPerfil(){
+    this.exibe = true;
+  }
 
+  public ocultarPerfil(){
+    this.exibe = false;
+  }
+  
 }
